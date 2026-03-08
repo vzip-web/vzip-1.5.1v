ef6f77ae2379bb242304e739f1b3b20eb72ba165
vzip-web <vzipgithub@gmail.com>
2026-03-09 03:44:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './contact.html',
+    './src/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
